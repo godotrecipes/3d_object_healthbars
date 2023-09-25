fcce673c96d7ad16febe952b782884a67f249d55
Chris Bradfield <cb@scribe.net>
2023-09-25 16:17:03 -0700

    remove warnings

--- health_bar_2d.gd
+++ health_bar_2d.gd
@@ -7,12 +7,12 @@ var bar_yellow = preload("res://barHorizontal_yellow_mid 200.png")
 func _ready():
 	hide()
 
-func update_health(_value, _max):
+func update_health(_value, _max_value):
 	value = _value
-	if value < _max:
+	if value < _max_value:
 		show()
 	texture_progress = bar_green
-	if value < 0.75 * _max:
+	if value < 0.75 * _max_value:
 		texture_progress = bar_yellow
-	if value < 0.45 * _max:
+	if value < 0.45 * _max_value:
 		texture_progress = bar_red
