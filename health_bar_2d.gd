05341e3a53c7ffc6f81e2ff86f6f00edcc63afab
Chris Bradfield <cb@scribe.net>
2023-09-25 16:14:42 -0700

    remove warnings

--- health_bar_2d.gd
+++ health_bar_2d.gd
@@ -7,12 +7,12 @@ var bar_yellow = preload("res://barHorizontal_yellow_mid 200.png")
 func _ready():
 	hide()
 
-func update_health(_value, max_value):
+func update_health(_value, _max):
 	value = _value
-	if value < max_value:
+	if value < _max:
 		show()
 	texture_progress = bar_green
-	if value < 0.75 * max_value:
+	if value < 0.75 * _max:
 		texture_progress = bar_yellow
-	if value < 0.45 * max_value:
+	if value < 0.45 * _max:
 		texture_progress = bar_red
--- health_bar_3d.gd
+++ health_bar_3d.gd
@@ -3,5 +3,5 @@ extends Sprite3D
 func _ready():
 	texture = $SubViewport.get_texture()
 	
-func update_health(_value, max_value):
-	$SubViewport/HealthBar2D.update_health(_value, max_value)
+func update_health(_value, _max_value):
+	$SubViewport/HealthBar2D.update_health(_value, _max_value)
